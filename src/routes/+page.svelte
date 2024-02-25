9dbfa6ad3d20afc86c00d62c4b0b7b35e0bdd987
infuriating <luca@infs.world>
2024-02-25 22:26:34 +0100

    Add User component to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import User from '../components/User.svelte';
+</script>
+
+<div class="min-h-screen bg-gradient-to-br from-purple-950/50 from-10% px-8">
+	<div
+		class="absolute bg-purple-900/25 blur-[100px] h-[75vh] aspect-square rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-0"
+	/>
+	<User />
+</div>
