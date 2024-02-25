1571ec1b15ae503945f07e333ccc4b794a1afaae
infuriating <luca@infs.world>
2024-02-25 22:56:02 +0100

    refactor infs.world link styling

--- src/components/Socials.svelte
+++ src/components/Socials.svelte
@@ -28,10 +28,12 @@
 	{/each}
 </div>
 
-<a target="_blank" href="https://infs.world">
-	<p
-		class="text-center my-2 font-medium w-max bg-gradient-to-r from-cyan-500 to-fuchsia-500 bg-clip-text text-transparent"
-	>
-		infs.world
-	</p>
-</a>
+<div class="bg-black/40 rounded-md w-max">
+	<a target="_blank" href="https://infs.world">
+		<p
+			class="px-2 py-1 text-center my-2 font-bold w-max bg-gradient-to-r from-cyan-500 to-fuchsia-500 bg-clip-text text-transparent"
+		>
+			infs.world
+		</p>
+	</a>
+</div>
