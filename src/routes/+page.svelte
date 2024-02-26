eb7ca430754a471ed0df9c6c2aa373a17e004a53
infuriating <luca@infs.world>
2024-02-26 11:22:43 +0100

    Add social media icons to user profile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,28 @@
 <script>
 	import User from '../components/User.svelte';
+	import RiLinkedinFill from '~icons/ri/linkedin-fill';
+	import RiInstagramFill from '~icons/ri/instagram-fill';
+	import RiGithubFill from '~icons/ri/github-fill';
+
+	const socials = [
+		{
+			url: 'https://linkedin.com/in/lucakuiper',
+			icon: RiLinkedinFill
+		},
+		{
+			url: 'https://instagram.com/infs.world',
+			icon: RiInstagramFill
+		},
+		{
+			url: 'https://github.com/infuriating',
+			icon: RiGithubFill
+		}
+	];
 </script>
 
 <div class="min-h-screen bg-gradient-to-br from-purple-950/50 from-10% px-8">
 	<div
 		class="absolute bg-purple-900/25 blur-[100px] h-[75vh] aspect-square rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-0"
 	/>
-	<User userId="161948341461057536" />
+	<User {socials} userId="161948341461057536" />
 </div>
