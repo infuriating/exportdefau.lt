5c5d829ab126dac4c0f1c9efc3491bf930e50f28
infuriating <luca@infs.world>
2024-03-04 16:33:34 +0100

    Add thanks route for requests

---
+++ src/routes/thanks/+page.svelte
@@ -0,0 +1,25 @@
+<div
+	class="min-h-screen flex flex-col gap-y-2 justify-center items-center bg-gradient-to-br from-purple-950/40 px-2"
+>
+	<h1 class="text-4xl font-semibold leading-none tracking-tight text-neutral-100">
+		Thanks for your request!
+	</h1>
+	<p class="text-sm text-neutral-300">
+		Your request has been submitted and will be reviewed shortly.
+	</p>
+	<div class="flex gap-x-4 pt-2">
+		<a
+			class="w-max h-10 px-4 py-2 border border-white/40 bg-white/0 hover:bg-white/10 hover:text-accent-foreground rounded-md text-sm font-medium transition-all"
+			href="/"
+		>
+			Go back</a
+		>
+		<a
+			target="_blank"
+			href="https://infs.world/contact"
+			class="w-max h-10 px-4 py-2 border border-white/40 bg-purple-300/20 hover:bg-purple-300/30 rounded-md text-sm font-medium transition-all"
+		>
+			Contact</a
+		>
+	</div>
+</div>
